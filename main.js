7083d490581271eec5fdd8fdd8e8b660d12aadb5
steph-d989 <steb_d@hotmail.com>
2024-05-28 16:38:16 +0200

    Variables introducidas

---
+++ main.js
@@ -0,0 +1,49 @@
+const arrayCoches = [
+    {
+        matricula: 123-E,
+        modelo: 'modelo1',
+        propietario:'Isa'
+    },
+    {
+        matricula: 321-E,
+        modelo: 'modelo2',
+        propietario: 'Peter'
+    },
+    {
+        matricula: 345-R,
+        modelo: 'modelo3',
+        propietario: 'Poshito'
+    },
+    {
+        matricula: 376-N,
+        modelo: 'modelo4',
+        propietario: 'La Choni'
+    },
+    {
+        matricula: 987-D,
+        modelo: 'modelo5',
+        propietario: 'Naty Peluso'
+    }
+]
+const arrayMultas= [
+    {
+        matricula: 123-E,
+        multa: ['multa1','multa2'],
+    },
+    {
+        matricula: 321-E,
+        multa: ['multa1'],
+    },
+    {
+        matricula: 345-R,
+        multa: [],
+    },
+    {
+        matricula: 376-N,
+        multa: ['multa1','multa2', 'multa3','multa4', 'multa5'],
+    },
+    {
+        matricula: 987-D,
+        multa: ['multa1'],
+    }
+]
